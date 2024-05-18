dc6c1c78218dd09c8e165e3ca9b2822073b5b8ae
octavian <ursachioctavian2@gmail.com>
2024-05-18 19:58:09 +0300

    Interfata Bleagă

---
+++ IP/ILoggerWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IP
+{
+    public interface ILoggerWriter
+    {
+        void WriteToLog(int vkCode, String path, TestUI form);
+
+    }
+}
---
+++ IP/TestUI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IP
+{
+    public partial class TestUI : Form
+    {
+        LoggerWriter log;
+        public TestUI(LoggerWriter log)
+        {
+            this.log = log;
+            InitializeComponent();    
+            KeyText.ReadOnly = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            log.SetStrategy(new VKCodeStrategy());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            log.SetStrategy(new ToStringStrategy());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            log.SetStrategy(new ToCharStrategy());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
